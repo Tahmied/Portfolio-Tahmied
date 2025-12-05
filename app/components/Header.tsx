bf62e3c6352055a3ed442a0bc61009fdee252dca
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-05 08:18:51 +0600

    - fixed header hamburger button not working

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
   const pathname = usePathname()
@@ -58,8 +58,13 @@ export default function Header() {
             <p className="logo-text">Tahmied</p>
           </Link>
 
-          <label className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-            <input type="checkbox" checked={menuOpen} readOnly />
+          <label className="hamburger" htmlFor="hamburger-checkbox">
+            <input
+              type="checkbox"
+              id="hamburger-checkbox"
+              checked={menuOpen}
+              onChange={(e) => setMenuOpen(e.target.checked)}
+            />
             <svg viewBox="0 0 32 32">
               <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
               <path className="line" d="M7 16 27 16"></path>

c5aeb83b92645c11b21992ba34500d16654e8356
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-05 17:01:19 +0600

    - updatd project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react'
 import Header from '../../components/Header'
 import '../globals.css'
+import Link from 'next/link'
 
 export default function Projects() {
   useEffect(() => {
@@ -20,54 +21,61 @@ export default function Projects() {
       <Header />
 
       <section className="experiments">
-        <div id="experiments-header" className="section-header">
-          <h1 className="section-title">Personal Projects</h1>
-          <p className="section-subtitle">
-            Some webapps that is made for my personal use
+        <div id="experiments-header" className="section-header !mt-8">
+          <h1 className="section-title"> Projects</h1>
+          <p className="section-subtitle !mt-2 !mb-4">
+            All My Projects
           </p>
         </div>
         <div className="experiments-container">
-          <div className="experiment-item">
-            <div
-              className="image-container"
-              style={{ backgroundImage: "url('/media/task management system.webp')" }}
-            >
-              <div className="text-overlay">
-                <div className="title">
-                  Task Management System Developed for my use
-                </div>
-                <div className="subtitle">Task Management System</div>
-              </div>
-            </div>
-          </div>
 
-          <div className="experiment-item">
-            <div
-              className="image-container"
-              style={{ backgroundImage: "url('/media/app store.png')" }}
-            >
-              <div className="text-overlay">
-                <div className="title">
-                  A place to store my apps
+          <Link href={'https://www.gimbalsinsider.com'}>
+            <div className="experiment-item">
+              <div
+                className="image-container"
+                style={{ backgroundImage: "url('/media/task management system.png')" }}
+              >
+                <div className="text-overlay">
+                  <div className="title">
+                    Gimbals Insider
+                  </div>
+                  <div className="subtitle">Custom Wordpress Theme</div>
                 </div>
-                <div className="subtitle">App Store</div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="experiment-item">
-            <div
-              className="image-container"
-              style={{ backgroundImage: "url('/media/front end gallary.png')" }}
-            >
-              <div className="text-overlay">
-                <div className="title">
-                  My Front End Projects
+          <Link href={'https://mentor-faf09.web.app/'}>
+            <div className="experiment-item">
+              <div
+                className="image-container"
+                style={{ backgroundImage: "url('/media/mentor.png')" }}
+              >
+                <div className="text-overlay">
+                  <div className="title">
+                    Mentor
+                  </div>
+                  <div className="subtitle">Learning Platform</div>
                 </div>
-                <div className="subtitle">Front End Gallary</div>
               </div>
             </div>
-          </div>
+          </Link>
+
+          <Link href={'https://premium.royal-kitchen.pl/'}>
+            <div className="experiment-item">
+              <div
+                className="image-container"
+                style={{ backgroundImage: "url('/media/royal-kitchen.png')" }}
+              >
+                <div className="text-overlay">
+                  <div className="title">
+                    Royal Kitchen
+                  </div>
+                  <div className="subtitle">Freelance Project</div>
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
       </section>
     </div>
